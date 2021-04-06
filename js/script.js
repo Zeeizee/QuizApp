6803ceaebabb4b5d2e396a71938c01f39d6d9ae0
Zeenat Parveen <zenizee563@gmail.com>
2021-04-07 02:40:34 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+const correctAnswers = ['A', 'A', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+const displayScore = document.querySelector('.student-score');
+
+console.log(displayScore);
+studentAnswers = [];
+form.addEventListener('submit', e => {
+
+
+    e.preventDefault();
+    let score = 0;
+    studentAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    console.log(correctAnswers);
+    console.log(studentAnswers);
+    studentAnswers.forEach((element, index) => {
+        if (element === correctAnswers[index]) {
+            score = score + 25
+        }
+
+
+    });
+    scrollTo(0, 0);
+    let i = 0
+    const timer = setInterval(() => {
+        console.log(i)
+        displayScore.querySelector('span').textContent = `${i}%`;
+        if (i === score) {
+
+            clearInterval(timer);
+        }
+        else {
+            i = i + 1;
+        }
+
+    }, 10)
+
+
+
+
+
+
+})
+// setTimeout(() => {
+
+
+
+// }, 3000);
+// let i = 0
+// const timer = setInterval(() => {
+//     console.log(i)
+//     if (i === 5) {
+
+//         clearInterval(timer);
+//     }
+//     else {
+//         i = i + 1;
+//     }
+
+// }, 10)
+
+
